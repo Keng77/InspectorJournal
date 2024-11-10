8eaf56fcd59ae1d18509e618a823f71b7ed5415c
KENG ᅠ ᅠᅠ ᅠ <Keng7767@outlook.com>
2024-11-10 14:39:33 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EnergyInspectorJournal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
